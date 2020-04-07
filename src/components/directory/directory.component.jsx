f31934ed0a341c5bc708ef06cae3f767a088f4e1
Manuel Sauceda <manuelsauc@herbalife.com>
2020-04-06 20:58:24 -0500

    adding more components

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './directory.styles.scss'
+
+class Directory extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+
+        }
+    }
+}
