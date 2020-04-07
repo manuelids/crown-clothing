4417b5eb3497846bef4f7eddba0eb0da23763032
Manuel Sauceda <manuel.sauceda21@gmail.com>
2020-04-06 21:53:02 -0500

    brocken our home page into smaller components, finished styling our menu item component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,40 +1,12 @@
 import React from 'react';
+import Directory from '../../components/directory/directory.component'
 import './homepage.styles.scss'
 
 const HomePage = () => (
     <div className="homepage">
-        <div className="directory-menu">
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">Hats</h1>
-                    <span className="subtitle">Shop Now</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">Jackets</h1>
-                    <span className="subtitle">Shop Now</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">Sweaters</h1>
-                    <span className="subtitle">Shop Now</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">Women</h1>
-                    <span className="subtitle">Shop Now</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">Man</h1>
-                    <span className="subtitle">Shop Now</span>
-                </div>
-            </div>
-        </div>
+        {
+            <Directory />
+        }
     </div>
 )
 
