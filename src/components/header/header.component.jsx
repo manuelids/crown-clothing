bd811d78e88e1e52a1483be4e0959abfacf193e5
Manuel Sauceda <manuelsauc@herbalife.com>
2020-04-11 20:05:48 -0500

    added cartIcon and cartDropdown components to the application, also implemented cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux'; //This is a higher order component
 
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import './header.styles.scss'
 
 
 //current User comes from the HOC connected at the bottom
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className="header">
         <Link className="logo-container" to="/">
             <Logo className="logo" />
@@ -26,13 +28,18 @@ const Header = ({ currentUser }) => (
                 ? <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
                 : <Link className="option" to="/signin">SIGN IN</Link>
             }
-        </div>
+            <CartIcon />
+            </div>
+        {
+            hidden ? null : <CartDropdown />
+        }
     </div>
 );
 
 //State is the rootState from the root reducer that maps all the user-reducers
-const mapStateToProps = (state) => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 });
 
 //Higher order component to connect the state value to this component
