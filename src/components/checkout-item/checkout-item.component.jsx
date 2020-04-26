e3b878538bdcd8de77645457b9fd0bb53623a171
Manuel Sauceda <manuel.sauceda21@gmail.com>
2020-04-26 17:59:10 -0500

    fixing the class name in checkout-item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -4,7 +4,7 @@ import './checkout-item.styles.scss';
 
 const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
     <div className="checkout-item">
-        <div class="image-container">
+        <div className="image-container">
             <img src={imageUrl} alt="item" />
         </div>
 
